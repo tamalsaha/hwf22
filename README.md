ea052741695431317aaabdd8258b6ec762ce56b7
Tamal Saha <tamal@appscode.com>
2021-05-01 15:07:20 -0700

    added-all

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # hwf
+
+```
+kubebuilder init --domain x-helm.dev --skip-go-version-check
+kubebuilder create api --group code --version v1alpha1 --kind Module
+```
---
+++ api/v1alpha1/module_types.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2021 AppsCode Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/agpl-3.0.txt>.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ModuleSpec defines the desired state of Module
+type ModuleSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Module. Edit module_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// ModuleStatus defines the observed state of Module
+type ModuleStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Module is the Schema for the modules API
+type Module struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ModuleSpec   `json:"spec,omitempty"`
+	Status ModuleStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ModuleList contains a list of Module
+type ModuleList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Module `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Module{}, &ModuleList{})
+}
